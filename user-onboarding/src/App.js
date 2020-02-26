67e5fadfaa5ffb940a294ed4fd032c54caff9f6d
unknown <ken.brandon@aol.com>
2020-02-26 13:09:46 -0700

    first commit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import InputForm from "./components/UserForm";
+
+function App() {
+  return (
+    <div className="App">
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
